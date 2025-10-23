7c98d2debab4466fe4ba6271559902601696a03c
Nikita <nekitosik.pav@gmail.com>
2025-10-23 12:19:25 +0700

    Добавлена команда получения статистики пользователя

---
+++ 2_Application/Commands/UserStat/UserStatCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MlkAdmin._2_Application.DTOs.Responses;
+
+namespace MlkAdmin._2_Application.Commands.UserStat
+{
+    public class UserStatCommand : IRequest<UserStatResponse>
+    {
+        public ulong UserId { get; set; }
+    }
+}
